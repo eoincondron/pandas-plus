2fd99e6c8e2d13c1f00fa32863ea7c8b181ac73b
Eoin Condron <econdr@gmail.com>
2025-07-24 11:22:56 +0100

    Automatically filter out non-numeric columns when aggregating a DataFrame

--- pandas_plus/groupby/core.py
+++ pandas_plus/groupby/core.py
@@ -18,6 +18,7 @@ from ..util import (
     convert_data_to_arr_list_and_keys,
     get_array_name,
     _null_value_for_array_type,
+    series_is_numeric,
 )
 
 ArrayCollection = (
@@ -260,6 +261,17 @@ class GroupBy:
         self, values: ArrayCollection, mask: Union[ArrayType1D, None]
     ):
         value_list, value_names = convert_data_to_arr_list_and_keys(values)
+        if isinstance(values, (pd.DataFrame)):
+            value_list, value_names = map(
+                list,
+                zip(
+                    *[
+                        (val, name)
+                        for val, name in zip(value_list, value_names)
+                        if series_is_numeric(val)
+                    ]
+                ),
+            )
 
         to_check = value_list + [self.group_ikey]
         if mask is not None:
